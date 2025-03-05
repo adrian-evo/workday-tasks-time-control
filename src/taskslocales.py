14578f49b547a19d2d5177682fefa83a10e80699
adrian-evo <avaidos@gmail.com>
2025-03-05 08:40:32 +0200

    Fix custom locales

--- src/taskslocales.py
+++ src/taskslocales.py
@@ -8,14 +8,27 @@ from runtrayicon import set_locale_from_vault_file
 class Translation:
     def __init__(self):
         set_locale_from_vault_file()
-        self.locale = os.getenv('LANG', 'en')
-        self.translation = None
+        self.template = None
+        self.custom = None
         
     def get(self, text):
-        if self.translation is None:
+        # load translations only once
+        if self.template is None:
+            locale = os.getenv('LANG', 'en')
             path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'locales')
-            self.translation = gettext.translation('template', localedir=path, languages=[self.locale])
-        return self.translation.gettext(text)
+            self.template = gettext.translation('template', localedir=path, languages=[locale])
+            # custom translations are optional
+            try:
+                self.custom = gettext.translation('custom', localedir=path, languages=[locale])
+            except FileNotFoundError:
+                self.custom = None
+
+        # get custom translation if available, otherwise use template translation
+        if self.custom is not None:
+            custom_text = self.custom.gettext(text)
+            if custom_text != text:
+                return custom_text
+        return self.template.gettext(text)
 
 # Robot Framework variable to be used as ${TRANS.get('Text')}
 TRANS = Translation()
@@ -49,34 +62,15 @@ if __name__ == '__main__':
     path_de = os.path.join(os.path.dirname(head), 'locales/de/LC_MESSAGES/')
 
     # compile english locale
-    # if custom translation exists, concatenate it with the template
     if os.path.exists(path_en + 'custom.po'):
         print('custom en translation exists')
-        with open(path_en + 'combined.po', 'w') as outfile:
-            with open(path_en + 'custom.po') as f1:
-                outfile.write(f1.read())
-            with open(path_en + 'template.po') as f2:
-                outfile.write(f2.read())
-        subprocess.run(["python", msgfmt, "-o", path_en + 'template.mo', path_en + 'combined.po'])
-        # delete the combined file
-        os.remove(path_en + 'combined.po')
-    else:
-        print('no en custom translation')
-        subprocess.run(["python", msgfmt, path_en + 'template.po'])
+        subprocess.run(["python", msgfmt, path_en + 'custom.po'])
+    subprocess.run(["python", msgfmt, path_en + 'template.po'])
 
     # compile german locale
     if os.path.exists(path_de + 'custom.po'):
         print('custom de translation exists')
-        with open(path_de + 'combined.po', 'w') as outfile:
-            with open(path_de + 'custom.po') as f1:
-                outfile.write(f1.read())
-            with open(path_de + 'template.po') as f2:
-                outfile.write(f2.read())
-        subprocess.run(["python", msgfmt, "-o", path_de + 'template.mo', path_de + 'combined.po'])
-        # delete the combined file
-        os.remove(path_de + 'combined.po')
-    else:
-        print('no de custom translation')
-        subprocess.run(["python", msgfmt, path_de + 'template.po'])
+        subprocess.run(["python", msgfmt, path_de + 'custom.po'])
+    subprocess.run(["python", msgfmt, path_de + 'template.po'])
 
     print('done')
